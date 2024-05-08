d6dcf4df24b3da4fc2b049a58967203811383744
Anton <erikssonanton94@gmail.com>
2024-05-08 13:10:38 +0200

    Lagt till ny väg för att hämta data från min databas med namn jobs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -13,9 +14,36 @@ app.use(cors());
 
 app.use("/api", authRoutes);
 
-app.get("/api/skyddad", authenticateToken, (req, res) => {
-    res.json({ message: "Skyddad route! "});
-})
+//Lägger till nya posts i min skyddade databas, detta är för test i TC
+app.post("/api/skyddad", authenticateToken, async (req, res) => {
+    try {
+        // Skapa ett nytt jobbobjekt med data från begäran
+        const newJob = new Job({
+            title: req.body.title,
+            description: req.body.description,
+        });
+
+        // Spara det nya jobbet i databasen
+        await newJob.save();
+
+        res.status(201).json({ message: "Jobbet har lagts till i databasen." });
+    } catch (error) {
+        console.error("Error adding job to database:", error);
+        res.status(500).json({ message: "Ett fel uppstod när jobbet skulle läggas till i databasen." });
+    }
+});
+
+app.get("/api/skyddad", authenticateToken, async (req, res) => {
+    try {
+        // Hämta alla jobb från databasen
+        const jobs = await Job.find();
+
+        res.json(jobs);
+    } catch (error) {
+        console.error("Error fetching jobs from database:", error);
+        res.status(500).json({ message: "Ett fel uppstod när jobben skulle hämtas från databasen." });
+    }
+});
 
 // Funktion för att skapa en ny samling "jobs" i databasen
 async function createJobsCollection() {
@@ -36,23 +64,32 @@ async function createJobsCollection() {
     }
 }
 
-//Validerar token
-function authenticateToken(req, res, next) {
+// Skapa en Mongoose-modell för "jobs"
+const Job = mongoose.model('Job', {
+    title: String,
+    description: String,
+});
+
+// Middleware för att verifiera JWT och skapa "jobs" collection
+async function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) res.status(401).json({ message: "Du har ej tillgång till denna route - token saknas."});
+    if (token == null) return res.status(401).json({ message: "Du har ej tillgång till denna route - token saknas."});
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, username) => {
         if (err) return res.status(403).json({ message: "Ogiltig JWT"});
 
         req.username = username;
 
+        // Skapa "jobs" collection om den inte redan finns
         await createJobsCollection();
 
         next();
     });
 }
+
+
 
 //Starta applikationen
 app.listen(port, () => {
